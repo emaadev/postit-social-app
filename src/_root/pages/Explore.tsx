26eee282b05db4b307a13fe7ab9cc03db2956123
EmanuelNu <emanuel.nu02@gmail.com>
2023-11-15 20:48:12 -0300

    debugging

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -7,13 +7,14 @@ import {
   useGetPosts,
   useSearchPosts,
 } from "@/lib/react-query/queriesAndMutations";
+import { Models } from "appwrite";
 import { useState, useEffect } from "react";
 
 import { useInView } from "react-intersection-observer";
 
 export type SearchResultProps = {
   isSearchFetching: boolean;
-  searchedPosts: any;
+  searchedPosts: Models.Document;
 };
 
 const SearchResults = ({
@@ -110,7 +111,7 @@ const Explore = () => {
           <p className="text-light-4 mt-10 text-center w-full">End of posts</p>
         ) : (
           posts.pages.map((item, index) => (
-            <GridPostList key={`page-${index}`} posts={item.documents} />
+            <GridPostList key={`page-${index}`} posts={item?.documents} />
           ))
         )}
       </div>
